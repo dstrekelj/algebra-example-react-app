4d630db7911cd9eed4c05a082ceb96043f15d007
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-03-28 20:49:45 +0200

    Add nanoid dependency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { nanoid } from "nanoid";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>algebra-example-react-app</h1>
+        <p>Your unique ID is: {nanoid()}</p>
         <a
           className="App-link"
           href="https://github.com/dstrekelj/algebra-example-react-app"
