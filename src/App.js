f4b9492ca746dc7ae434d362c7903b66fe62040d
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-03-30 19:29:31 +0200

    Add array helpers module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { idHelpers } from "./library/helpers";
+import { idHelpers, arrayHelpers } from "./library/helpers";
+
+console.log(arrayHelpers.getRandomElement([1, 2, 3]));
 
 function App() {
   return (
--- src/library/helpers/idHelpers.js
+++ src/library/helpers/idHelpers.js
@@ -3,7 +3,9 @@ import { nanoid } from "nanoid";
 export function generateId() {
   return nanoid();
 }
+
 const module = {
   generateId,
 };
+
 export default module;
