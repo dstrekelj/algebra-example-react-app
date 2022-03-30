e8c3f21aaa824a53caff390138190c2a64dbaa41
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-03-30 18:35:42 +0200

    Add helpers module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { nanoid } from "nanoid";
+import { generateId } from "./library/helpers";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>algebra-example-react-app</h1>
-        <p>Your unique ID is: {nanoid()}</p>
+        <p>Your unique ID is: {generateId()}</p>
         <a
           className="App-link"
           href="https://github.com/dstrekelj/algebra-example-react-app"
