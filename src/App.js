c7b3fa5078867d062f798da15639e8547e164c4e
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-03-30 21:03:34 +0200

    Add components folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { idHelpers, arrayHelpers } from "./library/helpers";
-
-console.log(arrayHelpers.getRandomElement([1, 2, 3]));
-
-function RepositoryLink() {
-  return (
-    <a className="App-link" href="https://github.com/dstrekelj/algebra-example-react-app">
-      GitHub Repository Link
-    </a>
-  );
-}
+import { idHelpers } from "./library/helpers";
+import { RandomValue } from "./components/RandomValue";
+import { RepositoryLink } from "./components/RepositoryLink";
 
 function App() {
   return (
@@ -20,6 +12,7 @@ function App() {
         <h1>algebra-example-react-app</h1>
         <p>Your unique ID is: {idHelpers.generateId()}</p>
         <RepositoryLink />
+        <RandomValue />
       </header>
     </div>
   );
