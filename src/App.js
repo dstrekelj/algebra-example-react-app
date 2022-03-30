0625dda53a2051ca3064da18a48895e8bb0bf246
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-03-30 20:26:26 +0200

    Add RepositoryLink component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { idHelpers, arrayHelpers } from "./library/helpers";
 
 console.log(arrayHelpers.getRandomElement([1, 2, 3]));
 
+function RepositoryLink() {
+  return (
+    <a className="App-link" href="https://github.com/dstrekelj/algebra-example-react-app">
+      GitHub Repository Link
+    </a>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -11,12 +19,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>algebra-example-react-app</h1>
         <p>Your unique ID is: {idHelpers.generateId()}</p>
-        <a
-          className="App-link"
-          href="https://github.com/dstrekelj/algebra-example-react-app"
-        >
-          GitHub Repository
-        </a>
+        <RepositoryLink />
       </header>
     </div>
   );
