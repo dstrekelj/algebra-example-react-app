6b99d994475a2ce8cea65fb9a4737e6938fdfab5
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-04-11 20:14:59 +0200

    Add LoginForm component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { idHelpers } from "./library/helpers";
 import { RepositoryLink } from "./components/RepositoryLink";
 import { Quiz } from "./components/Quiz";
 import { useState } from "react";
+import { LoginForm } from "./components/LoginForm";
 
 function App() {
   const [actionCount, setActionCount] = useState(0);
@@ -12,12 +13,14 @@ function App() {
     setQuizAnswer({ answers, id });
   };
   const handleStateChange = () => setActionCount((state) => state + 1);
+  const handleLogin = (formState) => console.log(formState);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Your action count is: {actionCount}</p>
         <RepositoryLink>View Repository</RepositoryLink>
+        <LoginForm onLogin={handleLogin} />
         {quizAnswer === null && (
           <Quiz id={id} onSubmit={handleSubmit} onStateChange={handleStateChange} />
         )}
