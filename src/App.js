512a4cf2e01687d3141e2febc82b857b4e0b16c7
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-04-11 21:10:04 +0200

    Add LoginForm logic and answers display

--- src/App.js
+++ src/App.js
@@ -8,23 +8,43 @@ import { LoginForm } from "./components/LoginForm";
 function App() {
   const [actionCount, setActionCount] = useState(0);
   const [quizAnswer, setQuizAnswer] = useState(null);
+  const [loginState, setLoginState] = useState(null);
   const id = idHelpers.generateId();
   const handleSubmit = (answers, id) => {
     setQuizAnswer({ answers, id });
   };
   const handleStateChange = () => setActionCount((state) => state + 1);
-  const handleLogin = (formState) => console.log(formState);
+  const handleLogin = (formState) => {
+    setLoginState(formState);
+  };
+
+  let answerComponents = null;
+
+  if (quizAnswer !== null) {
+    answerComponents = Object.keys(quizAnswer.answers).map((key) => {
+      return (
+        <div key={key}>
+          {key}: {quizAnswer.answers[key]}
+        </div>
+      );
+    });
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Your action count is: {actionCount}</p>
         <RepositoryLink>View Repository</RepositoryLink>
-        <LoginForm onLogin={handleLogin} />
-        {quizAnswer === null && (
+        {loginState === null && <LoginForm onLogin={handleLogin} />}
+        {loginState !== null && (
+          <div>
+            {loginState.name} ({loginState.email})
+          </div>
+        )}
+        {quizAnswer === null && loginState !== null && (
           <Quiz id={id} onSubmit={handleSubmit} onStateChange={handleStateChange} />
         )}
-        {quizAnswer !== null && <div>{JSON.stringify(quizAnswer)}</div>}
+        {quizAnswer !== null && answerComponents}
       </header>
     </div>
   );
