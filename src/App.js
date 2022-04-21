b82ece58b2121566cf23369c9295e5584c7dd348
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-04-21 20:52:51 +0200

    Add login and quiz routes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
-import { RepositoryLink } from "./components/RepositoryLink";
-import { Quiz } from "./components/Quiz";
-import { useContext, useState } from "react";
-import { LoginForm } from "./components/LoginForm";
-import { Timer } from "./components/Timer";
+import { useContext } from "react";
 import { AppContext } from "./contexts/AppContext";
-import { withLocale } from "./hoc/withLocale";
-import { Heading } from "./components/Heading";
 import { Paragraph } from "./components/Paragraph";
 import { Button } from "./components/Button";
-import { Routes, Route, Link, useParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { HelpPage } from "./pages/HelpPage";
-import { HomePage } from "./pages/HomePage";
-
-const LocalizedRepositoryLink = withLocale(RepositoryLink);
+import { LoginPage } from "./pages/LoginPage";
+import { QuizPage } from "./pages/QuizPage";
 
 function App() {
   const appState = useContext(AppContext);
-  console.log(appState);
-  const [actionCount, setActionCount] = useState(0);
-  const [quizAnswer, setQuizAnswer] = useState(null);
-  const [loginState, setLoginState] = useState(null);
-  const [finishTime, setFinishTime] = useState(null);
-
-  const handleSubmit = (answers, id) => setQuizAnswer({ answers, id });
-  const handleStateChange = () => setActionCount((state) => state + 1);
-  const handleLogin = (formState) => setLoginState(formState);
-  const handleTimerFinish = (time) => setFinishTime(time);
-
-  let answerComponents = null;
-
-  if (quizAnswer !== null) {
-    answerComponents = Object.keys(quizAnswer.answers).map((key) => {
-      return (
-        <div key={key}>
-          {key}: {quizAnswer.answers[key]}
-        </div>
-      );
-    });
-  }
 
   return (
     <div className="App">
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="help" element={<HelpPage />}>
-          <Route path=":topic" element={<HelpPage />} />
-        </Route>
-      </Routes>
       <header className="App-header">
-        <Heading element="h1" size="1">
-          Quiz Application
-        </Heading>
         <Paragraph element="div">
           {appState.translate("currentLocale")}: {appState.locale}
         </Paragraph>
@@ -63,27 +25,14 @@ function App() {
         <Button buttonType="ghost" onClick={() => appState.setLocale("de")}>
           DE
         </Button>
-        <Paragraph element="p">
-          <div>Your action count is: {actionCount}</div>
-          <RepositoryLink>View Repository</RepositoryLink>
-          <LocalizedRepositoryLink>View Repository</LocalizedRepositoryLink>
-        </Paragraph>
-        {loginState === null && <LoginForm onLogin={handleLogin} />}
-        {loginState !== null && (
-          <Paragraph element="div">
-            {loginState.name} ({loginState.email})
-          </Paragraph>
-        )}
-        {quizAnswer === null && loginState !== null && (
-          <>
-            <Timer onTick={(time) => console.log(time)} onFinish={handleTimerFinish} />
-            <Quiz onSubmit={handleSubmit} onStateChange={handleStateChange} />
-          </>
-        )}
-        {quizAnswer !== null && answerComponents}
-        {quizAnswer !== null && <Paragraph element="div">{quizAnswer.id}</Paragraph>}
-        {finishTime !== null && <Paragraph element="div">{finishTime} s</Paragraph>}
       </header>
+      <Routes>
+        <Route index element={<LoginPage />} />
+        <Route path="quiz" element={<QuizPage />} />
+        <Route path="help" element={<HelpPage />}>
+          <Route path=":topic" element={<HelpPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
